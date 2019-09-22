c6ed00c5fca15b44f89fa59cd736b0173bb0a0d4
Fizvlad <fizvlad@mail.ru>
2019-09-22 22:18:38 +0300

    Added small test

--- test/vkdonate_test.rb
+++ test/vkdonate_test.rb
@@ -2,8 +2,19 @@ require "test_helper"
 
 class VkdonateTest < Minitest::Test
 
+
   def test_version_number
     refute_nil Vkdonate::VERSION
+  end
+
+  def test_donates
+    puts "Please enter API key:"
+    api_key = STDIN.gets.chomp
+    client = Vkdonate::Client.new api_key
+
+    donates = client.donates
+    assert_instance_of Array, donates, "donates method must return Array"
+    assert_instance_of Vkdonate::Donate, donates.first, "donates method must return Donate objects" unless donates.empty?
   end
   
 end
