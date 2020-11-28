8e15bbb991530f028527e04df6c050442f5c872c
Fizvlad <fizvlad@mail.ru>
2020-11-28 20:58:16 +0300

    feat: specs

--- spec/vkdonate/client_spec.rb
+++ spec/vkdonate/client_spec.rb
@@ -3,5 +3,27 @@
 require 'spec_helper'
 
 RSpec.describe Vkdonate::Client do
-  let(:client) { described_class.new(api_key) }
+  let(:client) { described_class.new(api_key_stub) }
+  let(:api_key_stub) { 'stub' }
+
+  describe '#request' do
+    let(:call) { client.request(action_stub, **options_stub) }
+    let(:action_stub) { 'stub' }
+    let(:options_stub) { {} }
+
+    it do
+      expect(Vkdonate).to receive(:request).with(api_key_stub, action_stub, **options_stub)
+      call
+    end
+  end
+
+  describe '#donates' do
+    let(:call) { client.donates(**options_stub) }
+    let(:options_stub) { {} }
+
+    it do
+      expect(Vkdonate).to receive(:donates).with(api_key_stub, **options_stub)
+      call
+    end
+  end
 end
